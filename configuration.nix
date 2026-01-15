73dbd5b688bc36bd19a59ceae258747aa4ad659a
sudonym_ <bailorjacrey@gmail.com>
2026-01-14 21:12:16 -0600

    Remove vbox leftovers

--- configuration.nix
+++ configuration.nix
@@ -100,7 +100,6 @@
   programs.fish.enable = true;
 
   # temporary options set while developing this configuration in a vm
-  virtualisation.virtualbox.guest.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
